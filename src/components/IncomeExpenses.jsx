8dd4d0bc326663c2a18496d979dc962d8e65756d
JooYoo <jooyoo@outlook.com>
2021-02-16 11:06:10 +0100

    04_use_context_incomeexpenses

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 function IncomeExpenses() {
+  // get date from GlobalState
+  const { transactions } = useContext(GlobalContext);
+
+  // get amounts
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  // calc: income
+  const income = amounts
+    .filter((amount) => amount > 0)
+    .reduce((acc, curr) => acc + curr)
+    .toFixed(2);
+
+  // calc: expense
+  const expense = amounts
+    .filter((amount) => amount < 0)
+    .reduce((acc, curr) => acc + curr)
+    .toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+$0.00</p>
+        <p className="money plus">{income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">{expense}</p>
       </div>
     </div>
   );
