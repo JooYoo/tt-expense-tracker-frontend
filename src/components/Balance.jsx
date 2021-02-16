34aae746a3b8286188a7930813abdc63a4b32ef7
JooYoo <jooyoo@outlook.com>
2021-02-16 08:24:00 +0100

    03_use_context_balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 function Balance() {
+  // get global data
+  const { transactions } = useContext(GlobalContext);
+
+  // get amounts as array
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  // calc: total amounts
+  const total = amounts.reduce((acc, curr) => acc + curr).toFixed(2);
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1 id="balance">${total}</h1>
     </>
   );
 }
