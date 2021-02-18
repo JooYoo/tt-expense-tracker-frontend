0f9c96a3496ff79bdc1a042e7de7b095b6b1cd73
JooYoo <jooyoo@outlook.com>
2021-02-18 12:10:24 +0100

    10_add_transaction_to_api

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -52,11 +52,29 @@ const GlobalProvider = ({ children }) => {
     }
   };
 
-  const addTransaction = (transaction) => {
-    dispatch({
-      type: 'ADD_TRANSACTION',
-      payload: transaction,
-    });
+  const addTransaction = async (transaction) => {
+    // get ready the headers info for http-post
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      // add data to endpoint
+      const res = await axios.post('/api/v1/transactions', transaction, config);
+
+      // dispatch the data and
+      dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: res.data.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: err.response.data.error,
+      });
+    }
   };
 
   return (
